5c59d1109b26a861168f533bf87b92faec24c9ac
Daan Vervacke <daan.vervacke@proton.me>
2026-09-14 18:58:26 +0200

    Adapt set_day refresh test to new Schedules facade API

--- tests/test_isystem.py
+++ tests/test_isystem.py
@@ -426,12 +426,12 @@ async def test_isystem_set_day_refreshes_cached_schedule(mock_modbus_unit):
     mock_modbus_unit.holding.update({147: 0x0000, 148: 0xC000, 149: 0x0000})
     boiler = DiematicISystem(mock_modbus_unit)
     await boiler.async_update()
-    assert boiler.schedules.circuit_b_p4[1] == [(time(8, 0), time(9, 0))]
+    assert boiler.schedules.get_week("circuit_b_p4")[1] == [(time(8, 0), time(9, 0))]
 
     await boiler.schedules.set_day("circuit_b_p4", 1, [(time(10, 0), time(11, 0))])
     await boiler.async_update()
 
-    assert boiler.schedules.circuit_b_p4[1] == [(time(10, 0), time(11, 0))]
+    assert boiler.schedules.get_week("circuit_b_p4")[1] == [(time(10, 0), time(11, 0))]
 
 
 async def test_isystem_pooled_and_read_once_reads_stay_inside_windows(
